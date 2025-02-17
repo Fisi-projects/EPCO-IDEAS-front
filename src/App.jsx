1265d5116f75c994cea709c8a8d357d9e072c534
DvKirbo <cuetosalazarsebas@gmail.com>
2025-02-16 22:04:59 -0500

    basic router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import ClippedDrawer from './components/Sidebar/Sidebar';
 import {Box} from '@mui/material';
 import RequestsTable from './pages/requests'; // Importar la tabla
+import { Route, Routes } from 'react-router';
 import SignIn from './pages/login';
 
 
@@ -11,12 +12,18 @@ function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Box sx={{ display: 'flex', justifyContent:'center' }}>
-        <ClippedDrawer/>
-        <Box sx={{ marginTop: '70px', width: '100%'}}>
-        <RequestsTable /> {/* Mostrar la tabla */}
-        </Box>
-        </Box>
+        <Routes>
+          <Route path='/wasa' element={<h1>wasaaaa</h1>} />
+          <Route path='/login' element={<SignIn />} />
+          <Route path='/' element={
+            <Box sx={{ display: 'flex', justifyContent:'center' }}>
+            <ClippedDrawer/>
+            <Box sx={{ marginTop: '70px', width: '100%'}}>
+            <RequestsTable /> {/* Mostrar la tabla */}
+            </Box>
+            </Box>
+          }/>
+        </Routes>
       </ThemeProvider>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Modal from './components/modals/versolicitud.jsx' //PRUEBA PARA VER SOLICITUD
+import { BrowserRouter } from 'react-router'
+//import Modal from './components/modals/versolicitud.jsx' //PRUEBA PARA VER SOLICITUD
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </StrictMode>,
 )
