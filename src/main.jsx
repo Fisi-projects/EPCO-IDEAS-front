5aab975f4e3b7c589a8739cfa0902452c058fd4a
JhonaTLM <alfredo.bartolo@unmsm.edu.pe>
2025-02-25 19:03:35 -0500

    eliminate create

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Modal from './components/modals/versolicitud.jsx' //PRUEBA PARA VER SOLICITUD
+import Modal from './components/modals/eliminar' //PRUEBA PARA VER ELIMINAR
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
